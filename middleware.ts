c3ef51791974012c2a1f36f7b4da41a9038c0e24
ekohmitch-gif <Ekoh.mitch@gmail.com>
2026-08-19 15:54:17 -0400

    Update middleware configuration

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,7 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.[\\w]+$).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
