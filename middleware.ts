eb47796e9fbb789878906e82980475c7ca7eda83
ekohmitch-gif <Ekoh.mitch@gmail.com>
2026-08-19 15:28:49 -0400

    Fix Clerk auth.protect syntax in middleware

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+  '/api/generate(.*)',
+  '/api/checkout(.*)'
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.[\\w]+$spin).*)',
+    '/(api|trpc)(.*)',
+  ],
+};
